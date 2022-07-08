891a48323d6d84da82fa4289f3c0a54f015b1244
GrooT921 <rarapin96@gmail.com>
2022-07-08 19:51:12 +0200

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+
+const categoriesEl = document.querySelector("ul#categories");
+const countOfChildren = categoriesEl.children.length;
+console.log(`Number of categories: ${countOfChildren}`);
+
+// ==================================================================
+
+
+
